faac06f70d2ce74387f6d4d302cb51ace7e1d5ac
bergi <bergi@axolotlfarm.org>
2016-09-06 20:59:05 +0200

    force property object caching

--- test/Api.js
+++ test/Api.js
@@ -3,9 +3,9 @@
 'use strict'
 
 const assert = require('assert')
-// const context = require('../lib/context')
+const context = require('../lib/context')
 const Api = require('../lib/Api')
-// const SimpleRDF = require('simplerdf/lite')
+const SimpleRDF = require('simplerdf/lite').SimpleRDF
 
 function operationCallFactory (value) {
   return function () {
@@ -130,9 +130,8 @@ describe('Api', () => {
       assert.equal(typeof Api.attachCalls, 'function')
     })
 
-    /* it('should attach operations defined in API object', () => {
+    it('should attach operations defined in API object', () => {
       let api = new SimpleRDF(context)
-      let simple = new SimpleRDF({type: context.type}, 'http://example.org/subject')
 
       let supportedClass = api.child('http://example.org/Class')
       api.supportedClass.push(supportedClass)
@@ -141,11 +140,37 @@ describe('Api', () => {
       supportedOperation.method = 'GET'
       supportedClass.supportedOperation.push(supportedOperation)
 
-      simple.type = 'http://example.org/Class'
+      let simple = new SimpleRDF({type: context.type}, 'http://example.org/subject')
+      simple.type = simple.child('http://example.org/Class')
 
       Api.attachCalls(api, simple, operationCallFactory('test'))
 
-      console.log(simple)
-    }) */
+      assert.equal(simple.get, 'test')
+    })
+
+    it('should attach property operations defined in API object', () => {
+      let api = new SimpleRDF(context)
+
+      let supportedClass = api.child('http://example.org/Class')
+      api.supportedClass.push(supportedClass)
+
+      let property = api.child('http://example.org/property')
+
+      let supportedProperty = api.child()
+      supportedProperty.property = property
+      supportedClass.supportedProperty = supportedProperty
+
+      let supportedOperation = api.child()
+      supportedOperation.method = 'GET'
+      property.supportedOperation.push(supportedOperation)
+
+      let simple = new SimpleRDF({type: context.type, property: 'http://example.org/property'}, 'http://example.org/subject')
+      simple.type = simple.child('http://example.org/Class')
+      simple.property = simple.child('http://example.org/link')
+
+      Api.attachCalls(api, simple, operationCallFactory('test'))
+
+      assert.equal(simple.property.get, 'test')
+    })
   })
 })
