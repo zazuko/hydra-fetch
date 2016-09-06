3ab9246871868fa951d2913df774160721a52650
bergi <bergi@axolotlfarm.org>
2016-09-06 20:59:51 +0200

    removed workarounds for old SimpleRDF version

--- examples/light-switch.js
+++ examples/light-switch.js
@@ -11,10 +11,7 @@ let context = {
     '@id': 'http://example.org/power',
     '@type': '@id'
   },
-  status: {
-    '@id': 'http://example.org/status',
-    '@type': '@id'
-  },
+  status: 'http://example.org/status',
   type: {
     '@id': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
     '@type': '@id'
@@ -32,21 +29,21 @@ hydraFetch('http://localhost:8080/', {context: context}).then((lightSwitch) => {
   console.log('label: ' + lightSwitch.label)
 
   return lightSwitch.status.get().then((status) => {
-    console.log('current power status: ' + status.power.iri())
+    console.log('current power status: ' + status.power)
 
     console.log('switch on')
     return switchOnOff(lightSwitch.status, context.on)
   }).then(() => {
     return lightSwitch.status.get()
   }).then((status) => {
-    console.log('new power status: ' + status.power.iri())
+    console.log('new power status: ' + status.power)
 
     console.log('switch off')
     return switchOnOff(lightSwitch.status, context.off)
   }).then(() => {
     return lightSwitch.status.get()
   }).then((status) => {
-    console.log('new power status: ' + status.power.iri())
+    console.log('new power status: ' + status.power)
   })
 }).catch((err) => {
   console.error(err.stack || err.message)
